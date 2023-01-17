8e7eb441fa2c5157c73bf74f65346c3c605848fe
Ben Berman <bpb062004@gmail.com>
2023-01-17 12:53:21 -0600

    finished lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello! My name is Ben Berman");
+        System.out.println("I took some of your classes already last Summer");
+        System.out.println("My favorite 'sport' is bowling and I used to be on a team.");
+        System.out.println("I have finally caught up to the most recent release in One Piece, which is" +
+                "a show with over 1000 episodes");
+        System.out.println("I have started to train as a server at my job.");
+
     }
+
 }
