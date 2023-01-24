2742e9deda05a680721618b31311dc9f453b83c2
BennyBoo <bpb062004@gmail.com>
2023-01-23 21:13:00 -0600

    Add files via upload

---
+++ Variables/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    byte one = 1;
+    short two = 2;
+    int three = 80;
+    long four = 32323232;
+    float five = 3.14F;
+    double six = 20.980;
+    boolean seven = false;
+    private char x;
+    char demo = x;
+}
