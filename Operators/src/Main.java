34dfcd767b1178798faccd6de7fcced348bc37aa
BennyBoo <bpb062004@gmail.com>
2023-01-23 21:51:47 -0600

    Add files via upload

---
+++ Operators/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+   System.out.print("Adding: " + (6 + 6));
+   System.out.println("Subtracting: " + (10 - 9));
+   System.out.println("Multiplying: " + (8 * 8));
+   System.out.println("Dividing: " + (40 / 4));
+   System.out.println("Mod: " + (39 % 5));
+}
