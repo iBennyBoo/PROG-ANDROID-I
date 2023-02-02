5fd1a1aeb77f37b4e5c149aea364c357324b85cd
BennyBoo <bpb062004@gmail.com>
2023-02-02 14:05:51 -0600

    Add files via upload

---
+++ Arrays/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] supplies = {"buns", "patties", "mustard", "ketchup", "cheese", "tomatoes", "lettuce", "pickles"};
+        System.out.println("Printing out supplies for burger stand:");
+        for (int i = 0; i < supplies.length; i++) {
+            System.out.println(supplies[i]);
+        }
+    }
+}
