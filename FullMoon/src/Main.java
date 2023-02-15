6eddb3d63d87e011b8782a90cb1d8bf74d7357ef
BennyBoo <bpb062004@gmail.com>
2023-02-15 13:32:03 -0600

    Update Main.java

--- FullMoon/src/Main.java
+++ FullMoon/src/Main.java
@@ -6,14 +6,21 @@ public class Main {
     public static void main(String[] args) {
         int year = Calendar.getInstance().get((Calendar.YEAR));
         var formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        
+        //getting users system date
         var today = LocalDate.now();
+        
+        //date of next full moon
         var moon = LocalDate.of(2023,3,7);
+        
+        //formatted
         var formattedToday = today.format(formatter);
         var formattedMoon = moon.format(formatter);
 
         System.out.println("Today is " + formattedToday);
         System.out.println("Next Full Moon is on " + formattedMoon);
 
+        //days until full moon
         var daysToMoon = today.until(moon, ChronoUnit.DAYS);
         System.out.println("The next full moon is in " + daysToMoon + " days.");
 
